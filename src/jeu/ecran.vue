933edc823a01f1f8cee5d1dc140fdb8053a51f14
kyller92 <kyller92@hotmail.fr>
2018-05-24 12:36:20 +0200

    Plateau présent
    Il est tout bleu

---
+++ src/jeu/ecran.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="Plateau" >
+        <!-- <canvas width="300" height="300" style="border:1px solid #BBB;" v-insert-message="exampleContent"></canvas> -->
+        <div v-for="(horizon,index) in grille.squares" v-bind:key="index" :index="index">
+            <span class="cases" display: inline v-for="(vertic,index) in horizon" v-bind:key="index" :index="index">
+                {{vertic}}
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+import Board from '@/scripts/Board.js' 
+import design from '@/scripts/design.css'
+
+export default {
+  name: 'Ecran',
+  created(){
+    Board.init(4)
+    this.grille = Board
+    console.log(Board)
+    console.log(this.grille)
+  },
+  data () {
+    return {
+      grille:''
+    }
+  }
+}
+</script>
